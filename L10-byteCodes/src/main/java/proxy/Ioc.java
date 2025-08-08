31d2571f6ce5512ae2d68b30d1796e3834b9ae5c
ebalakina <elena.balakina@diabolocomc.com>
2025-08-08 21:53:53 +0700

    hw10-byteCodes

--- L10-byteCodes/src/main/java/proxy/Ioc.java
+++ L10-byteCodes/src/main/java/proxy/Ioc.java
@@ -1,28 +1,29 @@
 package proxy;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class Ioc {
 
     public static TestLoggingInterface createTestLogging() {
         TestLogging original = new TestLogging();
-        Set<String> loggableMethodSignatures = new HashSet<>();
+        Set<Method> loggableMethods = new HashSet<>();
 
-        for (Method method : original.getClass().getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Log.class)) {
-                loggableMethodSignatures.add(buildSignature(method));
+        for (Method interfaceMethod : TestLoggingInterface.class.getMethods()) {
+            try {
+                Method implMethod =
+                        original.getClass().getMethod(interfaceMethod.getName(), interfaceMethod.getParameterTypes());
+                if (implMethod.isAnnotationPresent(Log.class)) {
+                    loggableMethods.add(interfaceMethod);
+                }
+            } catch (NoSuchMethodException e) {
             }
         }
 
         InvocationHandler handler = new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                String signature = buildSignature(method);
-                if (loggableMethodSignatures.contains(signature)) {
+                if (loggableMethods.contains(method)) {
                     StringBuilder log = new StringBuilder("executed method: " + method.getName());
                     if (args != null && args.length > 0) {
                         for (int i = 0; i < args.length; i++) {
@@ -36,22 +37,8 @@ public class Ioc {
                 return method.invoke(original, args);
             }
         };
+
         return (TestLoggingInterface) Proxy.newProxyInstance(
                 Ioc.class.getClassLoader(), new Class<?>[] {TestLoggingInterface.class}, handler);
-    }
-
-    /**
-     * Builds a unique string signature for the given method based on its name and parameter types.
-     * This signature can be used to identify and compare methods across different classes/interfaces.
-     *
-     * @param method the method to generate a signature for
-     * @return a string representing the method's signature in the format: methodName|paramType1|paramType2...
-     */
-    private static String buildSignature(Method method) {
-        StringBuilder sb = new StringBuilder(method.getName());
-        for (Class<?> paramType : method.getParameterTypes()) {
-            sb.append("|").append(paramType.getName());
-        }
-        return sb.toString();
     }
 }
